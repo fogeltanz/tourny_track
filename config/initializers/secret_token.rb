13afb117ea4acaf190268febc241d712a37859d2
Blair Fogeltanz <fogeltanz@wisc.edu>
2014-04-12 20:43:58 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TournyTrack::Application.config.secret_key_base = '939f1e64312439be4d473d16fc0fbaabd11d19c110cf34ed83d55e6c60301d8fee7125f1b78e0245708bce89ad0dfb7e14ae7830e34c898af6558832e71fbaba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TournyTrack::Application.config.session_store :cookie_store, key: '_tourny_track_session'
