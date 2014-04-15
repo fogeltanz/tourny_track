3d3501161ca17ff635f9ac2fd95e1d3d238f9d2d
Blair Fogeltanz <fogeltanz@wisc.edu>
2014-04-15 10:31:50 -0500

    devise players

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 TournyTrack::Application.routes.draw do
+  devise_for :players
+  resources :players
+  
+  root :to => "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
